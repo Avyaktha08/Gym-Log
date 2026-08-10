d8b4ee8162e98293b10fb55aecf1724db655ae11
Avyaktha <avyaktha2168@gmail.com>
2026-08-10 22:13:09 +0530

    set add and display functions

--- script.js
+++ script.js
@@ -7,20 +7,35 @@ function addExercise(){
     const weightEle = document.querySelector('.weightElement');
     let weight = weightEle.value;
 
+    const repsEle = document.querySelector('.repsElement');
+    let reps = repsEle.value;
+
+    const setsEle = document.querySelector('.setsElement');
+    let sets = setsEle.value;
+
     exercises.push(
         {name : exerciseName,
-        weight : weight});
+        weight : weight,
+        reps: reps,
+        sets: sets});
 
     inputEle.value = "";
     weightEle.value = "";
+    repsEle.value="";
+    setsEle.value="";
 }
 
 let html='';
+let exerciseObject='';
 
 function display(){
-    exercises.forEach
-    //htmlCode='<p></p>'
-    //document.querySelector('.display').innerHTML=`html`;
+    exercises.forEach((value)=>{exerciseObject=value});
+    htmlCode=`<p>${exerciseObject.name} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+        Weight: ${exerciseObject.weight} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+        Reps: ${exerciseObject.reps} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+        Sets:${exerciseObject.sets}</p>`
+    html += htmlCode;
+    document.querySelector('.display').innerHTML=`${html}`;
 }
 
 const addButton=document.querySelector('.addButton')
